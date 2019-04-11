9058027d999b24825b7e865939a07d4d008fb149
wangshichao <1414585969@qq.com>
2019-04-11 17:13:35 +0800

    twice commit all.

--- src/com/hellogit/HellpGit.java
+++ src/com/hellogit/HellpGit.java
@@ -3,5 +3,6 @@ package com.hellogit;
 public class HellpGit {
     public static void main(String[] args) {
         System.out.println("Hello Git, I come from IDEA.");
+        System.out.println("hi Git,nice to meet to you.");
     }
 }
